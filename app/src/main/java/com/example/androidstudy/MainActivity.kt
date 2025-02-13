7df2157d0d451b4d220d1aaf8c349916acd3d587
“Roman <r.patsiarukha@gmail.com>
2025-02-13 09:00:46 +0300

    task2 commit

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.androidstudy
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Telephony.Mms.Intents
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity()
 {
@@ -17,6 +15,31 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val buttonForSwing = findViewById<Button>(R.id.btnGoToSecondActivity)
+
+        buttonForSwing.setOnClickListener(){
+            val secondActivity = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(secondActivity)
+
+        }
+
+
+        val numberSet = setOf(1,2,3,4,5,5,3)
+        Log.d("numberSet", numberSet.joinToString())
+
+        val mutNumberSet = mutableSetOf(1,2,4,5,9,5,7,3)
+        Log.d("mutNumberSet1", mutNumberSet.joinToString())
+
+        mutNumberSet.add(8)
+        mutNumberSet.remove(5)
+        Log.d("mutNumberSet2", mutNumberSet.joinToString())
+
+        val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3)
+        Log.d("numbersMap", numbersMap.entries.joinToString(", "))
+
+        val myList = listOf("Max","Mark","Alex")
+        Log.d("numbersMyList", myList.joinToString())
 
     }
 
---
+++ app/src/main/java/com/example/androidstudy/task2.kt
@@ -0,0 +1,8 @@
+package com.example.androidstudy
+
+fun main(){
+
+
+
+
+}
