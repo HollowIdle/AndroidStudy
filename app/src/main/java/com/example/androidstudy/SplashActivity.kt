cce4d999cbe34ff3fcfcd2cb45c1c89a16624ccc
“Roman <r.patsiarukha@gmail.com>
2025-02-24 19:35:44 +0300

    splash activity

---
+++ app/src/main/java/com/example/androidstudy/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidstudy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+    }
+}
