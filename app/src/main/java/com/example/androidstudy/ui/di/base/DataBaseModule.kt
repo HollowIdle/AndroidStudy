ab21764b756a76e6ac9352a35ac466c2a9204dab
“Roman <r.patsiarukha@gmail.com>
2025-04-07 20:31:31 +0300

    implemented database module

--- app/src/main/java/com/example/androidstudy/ui/di/base/DataBaseModule.kt
+++ app/src/main/java/com/example/androidstudy/ui/di/base/DataBaseModule.kt
@@ -1,11 +1,36 @@
 package com.example.androidstudy.ui.di.base
 
+import android.content.Context
+import androidx.room.Room
+import com.example.androidstudy.ui.data.local.PostEntity
+import com.example.androidstudy.ui.data.local.PostsDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataBaseModule {
-    //TODO will be implemented
+object DataBaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): PostsDatabase =
+        Room.databaseBuilder(
+            context,PostsDatabase::class.java,
+            "posts_db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideDao(db: PostsDatabase) = db.postsDao()
+
+    @Provides
+    fun provideEntity() = PostEntity()
 }
